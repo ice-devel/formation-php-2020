a64cba6006f1c57af36f5d2b91431bbf437e86fc
fab <fab@mail.Fr>
2020-10-02 16:40:01 +0200

    upload + exo

--- 1-initiation/14-upload.php
+++ 1-initiation/14-upload.php
@@ -12,8 +12,10 @@
         // $cv = filter_input(INPUT_POST, 'cv');
 
         $cv = $_FILES['cv'];
+        //$otherFile = $_FILES['other_file'];
 
-        // un fichier : 5 infos
+        // un fichier : 5 infos automatiquement récupérées par php
+        // https://www.php.net/manual/fr/features.file-upload.post-method.php
         $filename = $cv['name'];
         $type = $cv['type'];
         $tmpName = $cv['tmp_name'];
@@ -34,15 +36,18 @@
             $errors['size'] = "Le fichier est trop gros";
         }
 
+        // https://www.php.net/manual/fr/features.file-upload.errors.php
         if ($error != 0) {
             $errors['error'] = "Une autre erreur";
         }
 
         if (count($errors) == 0) {
+            // quel dossier ?
+            $folder = "uploads/";
+            // quel nom de fichier ?
+            $finalFilename = uniqid()."-".$filename;
             // copier le fichier depuis le dossier temporaire vers
             // le dossier final d'upload
-            $folder = "uploads/";
-            $finalFilename = uniqid()."-".$filename;
             move_uploaded_file($tmpName, $folder.$finalFilename);
         }
     }
@@ -69,12 +74,21 @@
     <p>
         Penser à indiquer l'attribut enctype pour autoriser l'upload
     </p>
-    
+
+    <p>
+        Configuration de PHP (dans le fichier php.ini) :
+        - upload_max_filesize : taille maximum autorisée par PHP pour un fichier uploadé.
+        Si un fichier est plus lourd, le tableau concernant ce fichier dans
+        la variable $_FILES aura des valeurs vides.
+        - post_max_size : taille totale de la requête POST autorisée
+        - memory_limit : mémoire vive maximum pour PHP
+    </p>
+
     <form method="post" enctype="multipart/form-data">
         <input type="text" name="name"/>
 
         <!-- il est possible de faire ça mais c'est un peu pourri
-        car il n'y a pas de message d'erreur, le fichier sera envoyé au serveur -->
+        car il n'y a pas de message d'erreur côté client, le fichier sera envoyé au serveur -->
         <input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
         <input type="file" name="cv" accept=".pdf,.jpg"/> (2mo max)
 
---
+++ 1-initiation/15-exo.php
@@ -0,0 +1,15 @@
+<?php
+   /*
+    * Créer un formulaire avec deux champs:
+    * Un pour le nom, un autre pour le pays (liste déroulante)
+    * Le formulaire doit être soumis en POST.
+    * Quand on recharge la page à la validation du formulaire,
+    * les champs doivent être pré-remplis avec les valeurs envoyées
+    * depuis la page précédente.
+    * Exemple : je saisi "toto" dans le champs "nom", je valide le formulaire :
+    * la page se recharge et "toto" est déjà inscrit dans le champs "nom".
+    * Idem pour le pays, j'ai choisi "France" dans la liste :
+    * quand la page se recharge, l'option doit être sélectionnée
+    * par défaut
+    */
+?>
